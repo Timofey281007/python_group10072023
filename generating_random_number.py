63e174278874b91d4229c1c6efb8a0f8160ad85b
Timofey281007 <tsoy281007@gmail.com>
2023-07-29 20:09:51 +0300

    'homework'

---
+++ generating_random_number.py
@@ -0,0 +1,5 @@
+import random
+
+
+def generate_random_number() -> int:
+    return random.randint(0, 1000)
---
+++ test_password_validation.py
@@ -0,0 +1,25 @@
+import pytest
+
+import password_validation
+
+test_cases_true = ['My_pwd23',
+                   'dC2+km2ekmmmmdk',
+                   '2=dldvvfvfve267248468',
+                   'Cho2o_________']
+
+test_cases_false = ['',
+                    'Mm2+',
+                    'Mn2+ pe2kepk',
+                    'ббггггG=2221',
+                    'ffffff2==',
+                    'FSFSFSFSDS2=-']
+
+
+@pytest.mark.parametrize('value', test_cases_true)
+def test_validate_password(value):
+    assert password_validation.validate_password(value)
+
+
+@pytest.mark.parametrize('value', test_cases_false)
+def test_validate_password(value):
+    assert not password_validation.validate_password(value)
